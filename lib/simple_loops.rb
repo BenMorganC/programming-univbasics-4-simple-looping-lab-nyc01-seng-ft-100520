cf9f21b9f2f907574fab402e88542f1c89e09892
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-16 19:07:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,6 +29,10 @@ def output_array(message)
 end
  
 
-def return_string_array
-
+def return_string_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array.to_s
+    counter +=1
+  end
 end
