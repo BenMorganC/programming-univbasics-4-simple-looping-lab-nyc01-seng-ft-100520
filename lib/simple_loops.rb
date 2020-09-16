bfddd7217dcd05eceb44bc6565c6ae17c940a3ac
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-16 18:37:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,15 +1,26 @@
 # Write your methods here
 
-def loop_message_five_times
-  
+counter = 0
+def loop_message_five_times(message)
+  while counter < 5
+  puts(message)
+  counter +=1
 end
-  
+ 
+ 
+ 
 def loop_message_n_times
   
 end
+ 
+ 
   
 def output_array
   
 end
+ 
+ 
   
 def return_string_array
+
+end
