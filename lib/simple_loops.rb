21f36b4996c3219bb34ffd82cacf13da6755740d
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-16 19:16:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -31,8 +31,9 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do
-    puts array[counter].to_s
+    puts array[counter].to_s << new_array
     return array[counter].to_s
     counter +=1   
    end
